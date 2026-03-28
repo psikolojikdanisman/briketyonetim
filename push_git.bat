3dcefb596ea196ab726ce75247247adf96bd9353
Mahmut Karadeniz <pd.mahmut@gmail.com>
2026-03-28 13:16:32 +0300

    $OutputEncoding = [System.Text.UTF8Encoding]::new()

---
+++ push_git.bat
@@ -0,0 +1,23 @@
+@echo off
+REM ===============================
+REM Tek tıkla Git commit + push
+REM ===============================
+
+REM Proje dizininde çalışıyor olmalı
+cd /d %~dp0
+
+REM Değişiklikleri ekle
+git add .
+
+REM Commit mesajı sor
+set /p msg="Commit mesajını girin: "
+
+REM Commit yap
+git commit -m "%msg%"
+
+REM Push
+git push
+
+echo.
+echo ✅ Push tamamlandı.
+pause
